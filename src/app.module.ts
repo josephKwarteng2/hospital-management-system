857464d919796e720dc5e574f7dd37aa60dcba16
josephKwarteng2 <josephkwarteng244@gmail.com>
2025-01-20 10:28:29 +0000

    feat: configure `MailerModule` to use environment variables for email credentials

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,40 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MailController } from './shared/mailer/mailer.controller';
+import { MailService } from './shared/mailer/mailer.service';
+import { configService } from './config/config.service';
+import { SeedModule } from './shared/seed/seed.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        transport: {
+          host: 'smtp.gmail.com',
+          port: 587,
+          secure: false,
+          auth: {
+            user: configService.get<string>('EMAIL_USER'),
+            pass: configService.get<string>('NODEMAILER_PASS'),
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    SeedModule,
+  ],
+  controllers: [AppController, MailController],
+  providers: [AppService, MailService],
 })
 export class AppModule {}
