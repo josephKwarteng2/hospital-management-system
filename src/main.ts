989e4395740a0be3c4701d524fa8e44a1d5cc715
josephKwarteng2 <josephkwarteng244@gmail.com>
2025-01-20 10:14:29 +0000

    chore: configure global pipes, global filters and global route prefix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SeederService } from './shared/seed/Admin-seeder.service';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './shared/common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      dismissDefaultMessages: false,
+      validationError: {
+        target: false,
+        value: false,
+      },
+    }),
+  );
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.setGlobalPrefix('api/v1');
+  const adminSeeder = app.get(SeederService);
+  await adminSeeder.createAdmin();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
