40d459a42c525501bd89063ae770c63db3037a08
josephKwarteng2 <josephkwarteng244@gmail.com>
2025-01-20 10:30:29 +0000

    feat: implement email invitation functionality in AppService

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { TOAST_MSGS } from './shared/constants/constants';
+import { BasicUserInfo } from './shared/types/types';
+import { InvitationDto } from './auth/dto/invitation.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('send-invitation')
+  async sendMail(@Query() query: InvitationDto): Promise<string> {
+    const { email, firstName, lastName } = query;
+
+    if (!email || !firstName || !lastName) {
+      throw new Error(TOAST_MSGS.MISSING_PARAMETERS);
+    }
+
+    const userDetails: BasicUserInfo = { email, firstName, lastName };
+    return await this.appService.sendInvitation(userDetails);
   }
 }
