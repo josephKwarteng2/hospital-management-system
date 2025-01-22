a6274409a1dcc1074bcd824f44e9732cd779399c
josephKwarteng2 <josephkwarteng244@gmail.com>
2025-01-22 11:25:48 +0000

    chore: cleanup app service and controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,6 @@
-import { Controller, Get, Query } from '@nestjs/common';
-import { AppService } from './app.service';
-import { TOAST_MSGS } from './shared/constants/constants';
-import { BasicUserInfo } from './shared/types/types';
-import { InvitationDto } from './auth/dto/invitation.dto';
+import { Controller } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get('send-invitation')
-  async sendMail(@Query() query: InvitationDto): Promise<string> {
-    const { email, firstName, lastName } = query;
-
-    if (!email || !firstName || !lastName) {
-      throw new Error(TOAST_MSGS.MISSING_PARAMETERS);
-    }
-
-    const userDetails: BasicUserInfo = { email, firstName, lastName };
-    return await this.appService.sendInvitation(userDetails);
-  }
+  constructor() {}
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,47 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { MailerService } from '@nestjs-modules/mailer';
-import * as jwt from 'jsonwebtoken';
-import { Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
-import { BasicUserInfo } from './shared/types/types';
-import { Invitation } from './shared/entities/invitations.entity';
-import { TOAST_MSGS } from './shared/constants/constants';
 
 @Injectable()
 export class AppService {
-  constructor(
-    private readonly mailService: MailerService,
-    @InjectRepository(Invitation)
-    private readonly invitationRepository: Repository<Invitation>,
-  ) {}
-
-  async sendInvitation(userDetails: BasicUserInfo): Promise<string> {
-    const token = jwt.sign(userDetails, process.env.JWT_SECRET, {
-      expiresIn: '24h',
-    });
-
-    const invitation = this.invitationRepository.create({
-      email: userDetails.email,
-      firstName: userDetails.firstName,
-      lastName: userDetails.lastName,
-      token,
-    });
-    await this.invitationRepository.save(invitation);
-
-    const invitationLink = `http://localhost:3000/register?token=${token}`;
-
-    try {
-      await this.mailService.sendMail({
-        to: userDetails.email,
-        from: 'josephkwarteng244@gmail.com',
-        subject: 'You’re invited to join the platform',
-        html: `<p>Hello ${userDetails.firstName} ${userDetails.lastName},</p>
-               <p>You have been invited to join our platform. Please click the link below to complete your account setup:</p>
-               <a href="${invitationLink}">Register</a>`,
-      });
-      return TOAST_MSGS.INVITATION_SENT;
-    } catch (error) {
-      return TOAST_MSGS.ERROR_SENDING_INVITATION;
-    }
-  }
+  constructor() {}
 }
