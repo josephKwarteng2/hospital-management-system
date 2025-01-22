8ae719be6ce517f5dcc651d69a0a1f47608c900d
josephKwarteng2 <josephkwarteng244@gmail.com>
2025-01-22 11:37:11 +0000

    chore: constants file

---
+++ src/shared/constants/constants.ts
@@ -0,0 +1,20 @@
+export const TOAST_MSGS = {
+  MISSING_PARAMETERS: 'Missing required parameters: email, firstName, lastName',
+  INVITATION_SENT: 'Invitation sent successfully',
+  ERROR_SENDING_INVITATION: 'Error sending invitation',
+  INVALID_ROLE: 'Invalid role. Role must be either doctor or patient',
+  EXPIRED_INVITATION: 'Invitation has expired',
+  INVALID_INVITATION_TOKEN: 'Invalid invitation token',
+  FAILED_TO_GENERATE_TOKEN: 'Failed to generate token',
+  INVITATION_USED: 'Invitation has already been used',
+  FAILED_TO_SEND_INVITATION: 'Failed to send invitation',
+  DEFAULT_PASSWORD_CHANGED_SUCCESSFULLY:
+    'Default password changed successfully. You can now proceed with registration',
+  DEFAULT_PASSWORD_UNCHANGED:
+    'Please change your default password before registration',
+  EMAIL_MISMATCH: 'Email does not match the invitation email',
+  ROLE_MISMATCH: 'Role does not match invitation',
+  DOCTOR_DETAILS_REQUIRED: 'Doctor details are required',
+  PATIENTS_DETAILS_REQUIRED: 'Patient details are required',
+  REGISTRATION_SUCCESSFUL: 'Registration successful',
+};
